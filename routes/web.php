e57454dc94de0e94121f65d49c99039e28152e22
Angela Tsurumaki <angelamutsumi@gmail.com>
2020-09-12 22:16:02 -0300

    Cria função update

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,4 @@ Route::get('/produtos/novo', [ProdutosController::class, 'create']);
 Route::post('produtos/novo', [ProdutosController::class, 'store'])-> name('registrar_produto');
 Route::get('/produto/ver/{id}', [ProdutosController::class, 'show']);
 Route::get('/produto/editar/{id}', [ProdutosController::class, 'edit']);
+Route::post('/produto/editar/{id}', [ProdutosController::class, 'update'])-> name('alterar_produto');
